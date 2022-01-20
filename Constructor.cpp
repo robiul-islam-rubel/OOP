443c51032cb0a3eba6aab496a93ca15cb0ab6b5a
Robiul Islam Rubel <77890232+rubel-47@users.noreply.github.com>
2022-01-20 23:47:47 +0600

    Add files via upload

---
+++ Constructor.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class myclass
+{
+public:
+    myclass()
+    {
+        cout<<"Constructor is called!"<<endl;
+    }
+};
+int main()
+{
+    myclass obj;
+    return 0;
+}
